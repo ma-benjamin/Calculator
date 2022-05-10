70b8e226456b0fa6d1dd68cf6525a5b1c5606746
Benjamin Ma <ben1002.ma@gmail.com>
2022-05-09 20:59:56 -0400

    Made the appearance of calculator

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function operator(op, a, b) {
+
+
+
+
+function operate(op, a, b) {
     if(op === '+') {
         return add(a, b)
     } else if(op === '-') {
@@ -11,6 +15,14 @@ function operator(op, a, b) {
         } else {
             return divide(a, b)
         }
+    } else if (op === "factorial") {
+        if(a < 0) {
+            return null
+        } else {
+            return factorial(a)
+        }
+    } else if (op === "exponent") {
+        return exponent(a, b)
     } else {
         return null
     }
@@ -32,3 +44,19 @@ function divide(a, b) {
     return a / b
 }
 
+function factorial(x) {
+    var product = 1
+    while(x > 1) {
+        product * x
+        x -= 1
+    }
+    return product
+}
+
+function exponent(a,b) {
+    var product = 1
+    for(var i = 0; i < b; i++) {
+        product * a
+    }
+    return product
+}
